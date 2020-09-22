fb9e9c3f705a8c0fd7fe5ac0109d4d78f3e7e437
Alessandro <alessandro.boscato@gmail.com>
2020-09-22 14:39:52 +0200

    inserita api key

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// Iscriviamoci al sito https://www.themoviedb.org. E’ completamente gratuito.
-// Richiediamo la nostra API_KEY che verrà utilizzata in tutte le nostre chiamate. Servirà all’API a capire chi sta effettuando la chiamata.
-// Per richiederla clicchiamo sul nostro user, poi impostazioni, API e clicchiamo su “Richiedi una nuova API key”.
-// Una volta generato, in Impostazioni / API avremo la nostra chiave, indispensabile per tutte le nostre chiamate.
-// Qua https://developers.themoviedb.org/3 troveremo tutte le chiamate possibili all’API. Possiamo giocarci in un secondo momento, ma come prima cosa concentriamoci su Search / Movies.
+// sito https://www.themoviedb.org.
+// API_KEY: 02a1201f97c8df2c585d649e1fd9e3fe
+// Servirà all’API a capire chi sta effettuando la chiamata.
+//https://developers.themoviedb.org/3 movie database
+
 // Milestone 1
 // Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
 // Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
@@ -12,5 +12,5 @@
 // Voto
 
 $(document).ready(function(){
-  
+
 });
