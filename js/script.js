d39727e11fb1775b693cc3f6909e332385d7792b
Alessandro <alessandro.boscato@gmail.com>
2020-09-23 15:55:09 +0200

    aggiunte flags

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
 // Titolo Titolo Originale Lingua Voto
 
 // Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5 e stampiamo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (FontAwesome).
 // Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
 // Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
 // Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
@@ -54,6 +54,7 @@ function callMovies() {
       "method": "GET",
       "success": function (data) {
         renderMovies(data.results);
+        console.log(data.results);
 
       },
       "error": function (richiesta, stato, errori) {
@@ -72,12 +73,11 @@ function renderMovies(movies) {
     var parsedVote = Math.round(movies[i].vote_average / 2);
     var starArray = [];
     checkStars(parsedVote, starArray);
-    console.log(starArray);
     var context = {
       "title": movies[i].title,
       "original_title": movies[i].original_title,
       "original_language": movies[i].original_language,
-      "vote_average": parsedVote,
+      "vote_average": movies[i].vote_average,
       "star": starArray
     }
     //stampa tutto l'html con tanti li quanti sono i film della ricerca
