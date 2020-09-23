339cb309318acd925fbc0adc1dcdb9732f4decd2
Alessandro <alessandro.boscato@gmail.com>
2020-09-23 17:31:02 +0200

    fine milestone 2

--- js/script.js
+++ js/script.js
@@ -12,15 +12,14 @@
 // Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5 e stampiamo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (FontAwesome).
 // Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
 // Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
-// Qui un esempio di chiamata per le serie tv:
+// Allarghiamola ricercaalle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 // https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
 
 $(document).ready(function(){
 // al click sul button
   $("#search_btn").click(
     function() {
-    $("#search_results").html("");
+    $("#movie_results").html("");
     callMovies();
     callSeries();
     $("#search_input").val("");
@@ -32,7 +31,8 @@ $(document).ready(function(){
       if (event.which == 13) {
         callMovies();
         callSeries();
-        $("#search_results").html("");
+        $("#movie_results").html("");
+        $("#search_input").val("");
       }
   });
 
@@ -106,7 +106,7 @@ function renderMovies(movies) {
     }
     //stampa tutto l'html con tanti li quanti sono i film della ricerca
     var html = template(context);
-    $("#search_results").append(html);
+    $("#movie_results").append(html);
   }
 }
 
@@ -127,11 +127,9 @@ function renderSeries(series) {
     }
     //stampa tutto l'html con tanti li quanti sono i film della ricerca
     var html = template(context);
-    $("#search_results").append(html);
+    $("#series_results").append(html);
   }
 }
-
-
 
 function checkStars(vote, array) {
   var i = 0;
