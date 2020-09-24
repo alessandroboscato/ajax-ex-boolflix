d4a7111788fb8e321ccbe63f6f4148939beecb6d
Alessandro <alessandro.boscato@gmail.com>
2020-09-24 14:52:42 +0200

    aggiunta funzione printposter

--- js/script.js
+++ js/script.js
@@ -16,9 +16,8 @@
 // https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
 
 // Milestone 3:
-// Aggiungiamo la copertina del film o della serie al nostro elenco.
-// Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
-// Dovremo prendere quindi l’URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la parte finale dell’URL passata dall’API.
+// Aggiungiamo la copertina del film o della serie. L'API ci passa solo la parte finale dell’URL, poi potremo generare da quella porzione di URL tante dimensioni diverse.
+// URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ + la dimensione che vogliamo (link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la parte finale dell’URL passata dall’API.
 // Esempio di URL che torna la copertina di PEPPA PIG:
 // https://image.tmdb.org/t/p/w185/tZgQ76hS9RFIwFyUxzctp1Pkz0N.jpg
 
@@ -48,6 +47,7 @@ $(document).ready(function(){
 });
 
 //-----------functions--------------
+
 
 function callMovies() {
   // prendo il valore dell'input
@@ -111,12 +111,18 @@ function renderMovies(movies) {
       "original_language": movies[i].original_language,
       "vote_average": movies[i].vote_average,
       "star": starArray,
-      "flag": movies[i].original_language
+      "flag": movies[i].original_language,
+      "poster": printPoster(movies[i])
     }
     //stampa tutto l'html con tanti li quanti sono i film della ricerca
     var html = template(context);
     $("#movie_results").append(html);
   }
+}
+
+function printPoster(content) {
+  var poster = "https://image.tmdb.org/t/p/w185/" + content.poster_path;
+  return poster
 }
 
 function renderSeries(series) {
