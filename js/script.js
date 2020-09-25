3a652c4b615662a1c01f1cea2cd7158a47c4bd78
Alessandro <alessandro.boscato@gmail.com>
2020-09-25 16:41:35 +0200

    Inizio milestone 6

--- js/script.js
+++ js/script.js
@@ -28,6 +28,12 @@
 // Andando con il mouse sopra una card (on hover), appaiono le informazioni aggiuntive già prese nei punti precedenti più la overview
 // p.s. per altre aggiunte grafiche siete liberi di copiare da Netflix, ma solo dopo aver finito la base.
 
+// Milestone 5 (Opzionale):
+// Partendo da un film o da una serie, richiedere all'API quali sono gli attori che fanno parte del cast aggiungendo alla nostra scheda Film / Serie SOLO i primi 5 restituiti dall’API con Nome e Cognome, e i generi associati al film con questo schema: “Genere 1, Genere 2, …”.
+// Milestone 6 (Opzionale):
+// Creare una lista di generi richiedendo quelli disponibili all'API e creare dei filtri con i generi tv e movie per mostrare/nascondere le schede ottenute con la ricerca.
+
+
 $(document).ready(function(){
 // al click sul button
   $("#search_btn").click(
